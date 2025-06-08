cf3321378ddc76eff55bb1939f03c49c93d31ea2
soumya007GitHub <130821564+soumya007GitHub@users.noreply.github.com>
2025-06-09 03:34:16 +0530

    Adding all files for learnyard backend using Strapi

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,9 +1,23 @@
 module.exports = [
-  'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+ 
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,19 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: 'dqxy5oxdb',
+        api_key: '737224655664861',
+        api_secret: '17szQ71CkAP4T7Y08XsRBBjB3Bs',
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+  // ...
+});
